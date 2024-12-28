567111d3cf1f7bf903482290ff88e3916fab461b
Henrik Hansen <hh@hhit.dk>
2024-12-28 21:41:37 +0100

    Deleted unused file

--- addons/unityplan/models/unityplan_forum.py.tmp.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-
-class ForumPost(models.Model):
-    _name = 'portal.forum.post'
-    _description = 'Forum Post'
-
-    content = fields.Text(required=True)
-    community_id = fields.Many2one('portal.community', string='Community', required=True)
